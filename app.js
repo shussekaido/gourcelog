4600f04065c9dcdefd39950077dcfdb0b79f9c10
Den <shussekaido@gmail.com>
2013-01-03 18:40:03 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,120 @@
+var readline = require('readline');
+var fs = require('fs');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+var timestamp = Math.round(+new Date / 1000); // Unix timestamp in seconds
+var username  = process.env.USER; // get shell username
+//var symbol    = ''; // 'A' for 'added', 'M' for modified
+var color     = ''; // To be determined at runtime
+
+// Variables for config file
+var configData = [];
+var configFile = __dirname + '/config.json';
+
+
+var checkTask = function(task, path, symbol) {
+
+  symbol = (symbol || 'M');
+
+  for (var i = 0, j = configData.length; i < j; i++) {
+    // Make search case-insensitive
+    var taskQuery = task.toLowerCase();
+    var taskInFile = configData[i].task.toLowerCase();
+    var parent = configData[i].parent;
+
+    // When the task is found
+    if (taskInFile === taskQuery) {
+      buildPath(configData[i].task, path, parent, symbol);
+      return;
+    }
+  }
+  processNewTask(task, path);
+}
+
+var processNewTask = function(task, path) {
+  rl.question('Enter name of parent task: ', function(parent) {
+    rl.prompt();
+    parent = parent.trim();
+    symbol = 'A';
+
+    switch(parent) {
+      case '':
+        configData.push({ "task": task });
+        saveConfig(configData);
+        // No parent, 'A' indicates new tasks in log
+        //path = task + (path || '');
+        buildPath(task, path, parent, symbol); // parent is 'null'
+        break;
+      default:
+        configData.push({ "task": task, "parent": parent });
+        saveConfig(configData);
+        // Parent defined, send task and path
+        path = '/' + task + (path || '');
+        checkTask(parent, path, symbol);
+        break;
+    }
+  });
+}
+
+var buildPath = function(task, path, parent, symbol) {
+  path = '/' + task + (path || '');
+  if (parent) {
+    checkTask(parent, path, symbol);
+  } else {
+    saveLog(path, symbol);
+  }
+}
+
+var saveLog = function(path, symbol) {
+  // Remove first slash (for proper log format)
+  var path = path.replace(/^\//, '');
+  // Append entry to log (NB: color is not implemented)
+  var entry = timestamp + '|' + username + '|' + symbol + '|' + path + '\n';
+
+  fs.appendFile(__dirname + '/gource.log', entry, encoding='utf8', function (err) {
+    if (err) throw err;
+  });
+}
+
+var saveConfig = function(configData) {
+
+  fs.writeFile(configFile, JSON.stringify(configData, null, 4), encoding='utf8', function(err){
+    if (err) throw err;
+  });
+}
+
+var init = function() {
+  // See if config file exists
+  if (fs.existsSync(configFile)) {
+    // read contents of config file
+    configData = JSON.parse(fs.readFileSync(configFile), encoding='utf8');
+    console.log('Loaded config.\n');
+  } else {
+    // or initialize new config file
+    saveConfig(configData);
+    console.log('Initialized an new config:\n', configFile, '\n');
+  }
+}();
+
+rl.setPrompt('Task > ');
+
+console.log('Hello, ' + username + '.');
+console.log('Enter task name: ');
+rl.prompt();
+
+rl.on('line', function (task) {
+  task = task.trim();
+  switch(task) {
+    case '':
+      break;
+    default:
+      checkTask(task);
+      break;
+  }
+  rl.prompt();
+}).on('close', function() {
+  console.log('\nHave a great day!');
+  process.exit(0);
+});
---
+++ readme.md
@@ -0,0 +1,80 @@
+# gourcelog #
+
+### Purpose ###
+
+Create custom personal logs for visualization with gource.
+
+Tested on OS X 10.7.4 with Node.js 0.8.16
+
+### Quick start ###
+
+* To run type: `node app.js`
+* Follow prompt to log tasks
+* Use gource to visualize your `gource.log`
+
+### How it works ###
+
+1. The script prompts the user to enter a task:
+
+Task > Vocabulary
+
+2. If the task exists it is simply logged with a timestamp (for example):
+
+`Study/Japanese/Vocabulary`
+
+otherwise a new prompt is shown:
+
+`Enter name of parent task > Japanese`
+`Enter name of parent task > Study`
+`Enter name of parent task >` "ENTER"  (no parent task, done)
+
+This records "Study/Japanese/Vocabulary" to a log file in gource format.
+
+Gource custom log format example:
+`1275543595|andrew|A|src/main.cpp`
+
+See [gource readme](https://github.com/acaudwell/Gource) for more info.
+
+Entered tasks are stored in config.json and the log is in gource.log. Both files are created at runtime. The script will automatically log new tasks with 'A' (added) and existing tasks 'M' (modified), which simply means "I worked on this task again" in our case.
+
+It's up to the user to use gource to create visualizations from the log.
+
+### How to use ###
+
+1. In the app directory run:
+`node app.js`
+
+2. Enter tasks and their parent tasks.
+
+3. Press "Enter" when done with a task
+
+To generate a basic gource file from the log produced by this program run
+`gource gource.log --stop-at-end --camera-mode track -1280x720 -o gource.ppm`
+
+Then convert it to a video:
+`ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i gource.ppm -vcodec libx264 -preset ultrafast -crf 1 -threads 0 -bf 0 gource.mp4`
+
+How to customize the look of videos: http://code.google.com/p/gource/wiki/Controls
+
+More info about creating videos: http://code.google.com/p/gource/wiki/Videos
+
+
+### Features ###
+
+* Each task and its "parent" (if one exists) is saved to config.json.
+When you type "Grammar" and then "Japanese" you will not need to enter "Study" as the parent task if you entered "Japanese" previously. The log entry will be "Study/Japanese/Grammar".
+
+
+
+### Other info ###
+
+Some additional features that could be implemented:
+
+* Auto-complete for existing tasks
+
+* Adding custom colors to log entries
+Gource custom log format allows specifying optional color (in hex):
+`1275543595|andrew|A|src/main.cpp|FF0000`
+
+# Author
+Shussekaido
